4ffc34101c90e4511806fcb7721bf69fd1e20294
urglmorl <urglmorl@urglmorl.dev>
2026-04-17 11:53:05 +0300

    Initial Commit

---
+++ openXTranscriber/Transcriber/TranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriberApp.swift
+//  Transcriber
+//
+//  Created by urglmorl on 17.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
